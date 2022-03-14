cea8b052fcbeaa1095ecba1dc57c223d7f2a0fb6
Lee Yang <leey@nvidia.com>
2022-03-14 16:33:53 -0700

    initial support for transformers.pipeline API

--- src/sparkext/huggingface/udf.py
+++ src/sparkext/huggingface/udf.py
@@ -1,57 +1,90 @@
 import pandas as pd
 import transformers
-import sentence_transformers
 
 from pyspark.sql.functions import pandas_udf
+# from transformers import pipeline
 from typing import Iterator
+
+optional_imports = []
+try:
+    import sentence_transformers
+    optional_imports.append("sentence_transformers")
+except:
+    pass
 
 
 def model_udf(model, model_loader=None, tokenizer=None, prefix=None, **kwargs):
+    # TODO: handle pipelines: https://huggingface.co/docs/transformers/master/en/main_classes/pipelines#transformers.pipeline
     # TODO: add prefix as transformer
     # TODO: add tokenizer as transformer
     # TODO: handle path to local cache
+    driver_model = None
+    driver_tokenizer = None
     if model_loader:
         print("Deferring model loading to executors.")
     elif type(model) is str:
         print("Loading {} model on driver".format(model))
-        m = transformers.AutoModel.from_pretrained(model)
-        t = transformers.AutoTokenizer.from_pretrained(model)
-    elif isinstance(model, (transformers.PreTrainedModel, sentence_transformers.SentenceTransformer)):
-        m = model
+        driver_model = transformers.AutoModel.from_pretrained(model)
+        driver_tokenizer = transformers.AutoTokenizer.from_pretrained(model)
+    elif isinstance(model, transformers.PreTrainedModel):
+        driver_model = model
+        driver_tokenizer = tokenizer
+    elif isinstance(model, transformers.pipelines.Pipeline):
+        driver_model = model
+    elif "sentence_transformers" in optional_imports and isinstance(model, sentence_transformers.SentenceTransformer):
+        driver_model = model
     else:
         raise ValueError("Unsupported model type: {}".format(type(model)))
 
     # TODO: cache model on executors
-    # TODO: support more flexible input/output types
-    @pandas_udf("string")
-    def predict_string(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
+    def predict_model(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
         if model_loader:
             print("Loading model on executors from: {}".format(model))
             executor_model = model_loader(model)
         else:
-            executor_model = m
+            executor_model = driver_model
+
+        executor_tokenizer = driver_tokenizer
 
         for batch in data:
             input = [prefix + s for s in batch.to_list()]
-            input_ids = tokenizer(input, **kwargs).input_ids
+            input_ids = executor_tokenizer(input, **kwargs).input_ids if executor_tokenizer else input
             output_ids = executor_model.generate(input_ids)
-            output = [tokenizer.decode(o, **kwargs) for o in output_ids]
+            output = [executor_tokenizer.decode(o, **kwargs) for o in output_ids] if executor_tokenizer else output_ids
             yield pd.Series(list(output))
 
-    @pandas_udf("array<float>")
-    def predict_floats(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
+    def predict_pipeline(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
         if model_loader:
             print("Loading model on executors from: {}".format(model))
             executor_model = model_loader(model)
         else:
-            executor_model = m
+            executor_model = driver_model
+
+        for batch in data:
+            input = [s for s in batch.to_list()]
+            output = executor_model(input)
+            yield pd.Series([x['label'] for x in output])
+
+    def predict_sentence_transformer(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
+        if model_loader:
+            print("Loading model on executors from: {}".format(model))
+            executor_model = model_loader(model)
+        else:
+            executor_model = driver_model
 
         for batch in data:
             input = [s for s in batch.to_list()]
             output = executor_model.encode(input)
             yield pd.Series(list(output))
 
-    if isinstance(model, transformers.PreTrainedModel):
-        return predict_string
-    elif isinstance(model, sentence_transformers.SentenceTransformer):
-        return predict_floats
+    if isinstance(driver_model, transformers.PreTrainedModel):
+        if driver_tokenizer:
+            return pandas_udf(predict_model, "string")
+        else:
+            return pandas_udf(predict_model, "array<int>")
+    elif isinstance(driver_model, transformers.pipelines.Pipeline):
+        return pandas_udf(predict_pipeline, "string")
+    elif "sentence_transformers" in optional_imports and isinstance(driver_model, sentence_transformers.SentenceTransformer):
+        return pandas_udf(predict_sentence_transformer, "array<float>")
+    else:
+        raise ValueError("Unsupported model type: {}".format(type(driver_model)))
