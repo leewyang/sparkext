3279605caebb17f6f17020fbd7df54f0aba97b69
Lee Yang <leey@nvidia.com>
2022-03-17 15:05:46 -0700

    support multiple input/output cols in tf

--- src/sparkext/tensorflow/udf.py
+++ src/sparkext/tensorflow/udf.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 import tensorflow as tf
 
 from dataclasses import dataclass
@@ -19,23 +20,35 @@ udf_types = {
     tf.string: "str"
 }
 
-@dataclass(frozen=True)
-class ModelSummary:
+class ModelSummary():
+    """Helper class to get basic model metadata w/o serializing model."""
     num_params: int
-    input: tuple
-    output: tuple
+    inputs: list[tf.Tensor]
+    outputs: list[tf.Tensor]
 
-def summary(model: tf.keras.Model):
-    # TODO: support multiple inputs/outputs
-    input0 = model.inputs[0]
-    output0 = model.outputs[0]
-    input = (input0.shape, input0.dtype)
-    output = (output0.shape, output0.dtype)
-    num_params = model.count_params()
-    return ModelSummary(num_params, input, output)
+    def __init__(self, model: tf.keras.Model):
+        self.num_params = model.count_params()
+        self.inputs = model.inputs
+        self.outputs = model.outputs
+
+    def __repr__(self) -> str:
+        return "ModelSummary(num_params={}, inputs={}, outputs={})".format(self.num_params, self.inputs, self.outputs)
+
+    def return_type(self, names=False) -> str:
+        def type_str(tensor: tf.Tensor) -> str:
+            udf_type = udf_types[tensor.dtype]
+            name = re.split('[/:]', tensor.name)[0]
+            tensor_type = f"array<{udf_type}>" if len(tensor.shape) > 0 else udf_type
+            return f"{name} {tensor_type}" if names else f"{tensor_type}"
+
+        output_types = [type_str(output) for output in self.outputs]
+        final_string = ', '.join(output_types)
+        print(f"return_type: {final_string}")
+        return final_string
 
 def model_udf(model: Union[str, tf.keras.Model],
               model_loader: Optional[Callable] = None,
+              input_columns: list[str] = None,
               **kwargs):
     # TODO: handle plain saved_models
     driver_model = None
@@ -53,13 +66,9 @@ def model_udf(model: Union[str, tf.keras.Model],
         raise ValueError("Unsupported model type: {}".format(type(model)))
 
     # get model input_shape and output_type
-    model_summary = summary(driver_model)
+    model_summary = ModelSummary(driver_model)
     print(model_summary)
-    input_shape = list(model_summary.input[0])
-    input_shape[0] = -1
-    output_shape = model_summary.output[0]
-    output_type = udf_types[model_summary.output[1]]
-    output_type = "array<{}>".format(output_type) if len(output_shape) > 0 else output_type
+    output_type = model_summary.return_type()
 
     # clear the driver_model if using model_loader to avoid serialization/errors
     if model_loader:
@@ -67,7 +76,7 @@ def model_udf(model: Union[str, tf.keras.Model],
 
     # TODO: infer input cols
     # TODO: input/output tensor support
-    def predict(data: Iterator[pd.Series]) -> Iterator[pd.Series]:
+    def predict(data: Iterator[pd.DataFrame]) -> Iterator[pd.DataFrame]:
         if model_loader:
             print("Loading model on executors from: {}".format(model))
             executor_model = model_loader(model)
@@ -75,7 +84,21 @@ def model_udf(model: Union[str, tf.keras.Model],
             executor_model = driver_model
 
         for batch in data:
-            input = np.vstack(batch).reshape(input_shape)
+            # first, check if the number of inputs matches expected
+            if input_columns:
+                num_expected = len(input_columns)
+                num_actual = len(batch)
+                assert num_actual == num_expected, "Model expected {} inputs, but received {}".format(num_expected, num_actual)
+                # create a dictionary of named inputs if input_columns provided
+                input = dict(zip(input_columns, batch))
+            else:
+                # vstack the batch if only one input expected
+                input_shape = list(executor_model.inputs[0].shape)
+                input_shape[0] = -1         # replace None with -1 in batch dimension for numpy.reshape
+                input = np.vstack(batch).reshape(input_shape)
+                input = np.vstack(batch)
+
+            # predict and return result
             output = executor_model.predict(input)
             yield pd.Series(list(output))
 
