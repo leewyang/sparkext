b3e8e4aef10e7a02ff1c1cc2dfa59e7ee31c1d85
Lee Yang <leey@nvidia.com>
2022-03-29 16:20:22 -0700

    updated cache tests

--- examples/cache_test/cache_pudf2.py
+++ examples/cache_test/cache_pudf2.py
@@ -18,3 +18,17 @@ def addN1_dist2(x: pd.Series) -> pd.Series:
         time.sleep(delay)
         print("{} pandas_udf done".format(threading.get_ident()))
     return x + N
+
+def get_fn():
+    def _addN1_dist2(x: pd.Series) -> pd.Series:
+        global N
+        if N:
+            print("USING CACHE!!!")
+            N = N + 1.0
+        else:
+            print("{} pandas_udf load".format(threading.get_ident()))
+            N = 1.0
+            time.sleep(delay)
+            print("{} pandas_udf done".format(threading.get_ident()))
+        return x + N
+    return _addN1_dist2
